f3956fc526a6ddeb58a3332de78386c83dc265bf
theyseemeRobin <100965324+theyseemeRobin@users.noreply.github.com>
2022-03-13 12:32:42 +0100

    Add files via upload

---
+++ infixExp.h
@@ -0,0 +1,10 @@
+/* prefixExp.h, Gerard Renardel, 29 January 2014 */
+
+#ifndef INFIXEXP_H
+#define INFIXEXP_H
+
+/* Here the definition of the type tree of binary trees with nodes containing tokens.
+ */
+void infixExpTrees();
+
+#endif
---
+++ mainInfix.c
@@ -0,0 +1,10 @@
+/* mainPref.c for lab assignment 4 expressions, updated 2021 */
+
+#include "scanner.h"
+#include "prefixExp.h"
+#include "infixExp.h"
+
+int main(int argc, char *argv[]) {
+  infixExpTrees();
+  return 0;
+}
